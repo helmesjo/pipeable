e8d60d91380cc980b73b7a557848e648f39a7981
helmesjo <helmesjo@gmail.com>
2019-11-06 20:52:45 +0100

    Added more complex data_generator test where generater pipe to interceptor which in turn pipe to receiver.

--- tests/data_generator_tests.cpp
+++ tests/data_generator_tests.cpp
@@ -2,6 +2,7 @@
 #include <pipeable/data_generator.hpp>
 
 #include <catch2/catch.hpp>
+#include <variant>
 
 using namespace pipeable;
 
@@ -15,6 +16,19 @@ namespace
             return val;
         }
         bool receivedValue = false;
+    };
+    struct int_and_string_receiver
+    {
+        int receivedInt = 0;
+        std::string receivedStr = "";
+        void operator()(int val)
+        {
+            receivedInt = val;
+        }
+        void operator()(const std::string& val)
+        {
+            receivedStr = val;
+        }
     };
 }
 
@@ -31,12 +45,38 @@ SCENARIO("Compose pipelines with a data generator")
             {
                 REQUIRE(receiver.receivedValue == false);
             }
-            AND_WHEN("data is piped to generator")
+            AND_WHEN("generator is invoked with data")
             {
-                1 >>= generator;
+                generator(1);
                 THEN("it is forwarded to the downstream pipeline")
                 {
                     REQUIRE(receiver.receivedValue == true);
+                }
+            }
+        }
+    }
+    GIVEN("a data generator outputting variant<x, y>")
+    {
+        data_generator<std::variant<int, std::string>> generator;
+
+        WHEN("it is piped as: generator >>= visit >>= receiver")
+        {
+            int_and_string_receiver receiver;
+            generator >>= visit >>= &receiver;
+            AND_WHEN("generator is invoked with x")
+            {
+                generator(1);
+                THEN("receiver::operator(x) is invoked")
+                {
+                    REQUIRE(receiver.receivedInt == 1);
+                }
+            }
+            AND_WHEN("generator is invoked with y")
+            {
+                generator("hello");
+                THEN("receiver::operator(y) is invoked")
+                {
+                    REQUIRE(receiver.receivedStr == "hello");
                 }
             }
         }
